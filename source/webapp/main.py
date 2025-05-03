ed54d3ca3bb32143b6f50f5819541f824762961e
Eugen <eugen.hoppe.de@gmail.com>
2025-05-03 22:11:45 +0200

    mount static files

--- source/webapp/main.py
+++ source/webapp/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
-from webapp.settings import dev_docs
+from webapp.settings import dev_docs, BASE_DIR
 from webapp.apps.api.app import api
 from webapp.apps.web.app import web
 
@@ -8,5 +9,6 @@ from webapp.apps.web.app import web
 app = FastAPI(title="WebApp", **dev_docs)
 
 
+app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
 app.mount("/api", api)
 app.mount("/", web)
